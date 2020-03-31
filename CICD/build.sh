c1b67845388bacb0e9c7e2961ec91031e05226f9
Rodrigo de Souza <rsouza01@gmail.com>
2020-03-31 22:45:53 +0200

    Removing TTY from build

--- CICD/build.sh
+++ CICD/build.sh
@@ -7,7 +7,11 @@ set -e
 
 docker build -f ./CICD/Dockerfile.tests -t astroimages-file-drivers:latest .
 
-docker run --rm -ti -p 5000:5000 astroimages-file-drivers
+# LOCAL VERSION
+# docker run --rm -ti -p 5000:5000 astroimages-file-drivers
+
+# GIT ACTION VERSION
+docker run --rm -p 5000:5000 astroimages-file-drivers
 
 # BASH-OPENING VERSON
 # docker run --rm -ti -p 5000:5000 astroimages-file-drivers /bin/bash
