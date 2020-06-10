4c5a26752d4cb381e5e10a27d6527585398f4417
Rodrigo de Souza <rsouza01@gmail.com>
2020-06-11 01:09:08 +0200

    Component tests passing

--- astroimages_file_drivers/drivers/local_file_driver.py
+++ astroimages_file_drivers/drivers/local_file_driver.py
@@ -16,6 +16,7 @@ class LocalFileDriver(GenericFileDriver):
 
     def store_files(self, folder_name, files, overwrite=False):
         for file in files:
+            # print('FILE = %s' % '{}/{}'.format(folder_name, file['name']))
             self.store_file('{}/{}'.format(folder_name, file['name']), file['contents'], overwrite)
 
     def store_file(self, path, file, overwrite=False):
--- test/component/test_file_system.py
+++ test/component/test_file_system.py
@@ -1,6 +1,6 @@
 import unittest
+import os
 
-from unittest import mock
 import astroimages_file_drivers.drivers.local_file_driver as local_file_driver
 
 
@@ -20,18 +20,6 @@ temp_files = [
 temp_file_contents = "SIMPLE  =                    T / file does conform to FITS standard"
 
 
-def mock_list_files_in_folder(folder, extension):
-    return temp_files
-
-
-def mock_store_file(path, file, overwrite=False):
-    pass
-
-
-def mock_read_full_file_in_bytes(path):
-    return bytearray(temp_file_contents, 'utf-8')
-
-
 class TestLocalFileSystem(unittest.TestCase):
 
     def setUp(self):
@@ -40,35 +28,42 @@ class TestLocalFileSystem(unittest.TestCase):
     def tearDown(self):
         pass
 
-    @mock.patch('astroimages_file_drivers.drivers.local_file_driver.list_files_in_folder',
-                side_effect=mock_list_files_in_folder)
-    @mock.patch('astroimages_file_drivers.drivers.local_file_driver.read_full_file_in_bytes',
-                side_effect=mock_read_full_file_in_bytes)
-    @mock.patch('astroimages_file_drivers.drivers.local_file_driver.store_file',
-                side_effect=mock_store_file)
-    def test_happyFlow_LocalFileHandler(self, mock_list_files_in_folder, mock_read_full_file_in_bytes, mock_store_file):
-        '>>>>>>>>> HAPPY FLOW'
-        driver = local_file_driver.LocalFileDriver()
-        # Retrieves the files in the folder
-        files = driver.get_files('folder_name', '.fits')
+    def test_happyFlow_LocalFileHandler(self):
+        'HAPPY FLOW'
+        folder_name = './test/data/'
 
-        # Fetches the first element
-        file = driver.get_physical_file(files[0])
+        try:
+            driver = local_file_driver.LocalFileDriver()
+            # Retrieves the files in the folder
+            files = driver.get_files(folder_name, '.fits')
 
-        print('\nFILE: %s' % files[0])
-        print('FILE CONTENTS: \'%s\'' % str(file))
-        print('FILE LEN: %d' % len(file))
+            # Fetches the first element
+            file = driver.get_physical_file(files[0])
 
-        array_files = [
-            {'name': 'file1.txt', 'contents': 'CONTENTS FILE1'},
-            {'name': 'file2.txt', 'contents': 'CONTENTS FILE2'},
-        ]
+            contents = bytearray(files[0], 'utf-8')
 
-        # If it does not raise an exception, it passed
-        driver.store_files('local_folder', array_files)
+            array_files = [
+                {'name': 'file1.abc', 'contents': contents},
+                {'name': 'file2.abc', 'contents': contents},
+            ]
 
-        self.assertEqual(len(files), len(temp_files), "Should be %s" % len(temp_files))
-        self.assertEqual(len(file), len(temp_file_contents), "Should be %s" % len(temp_files))
+            # If it does not raise an exception, it passed
+            driver.store_files(folder_name, array_files)
+
+            # Retrieves the newly created files
+            secondary_files = driver.get_files(folder_name, '.abc')
+
+            self.assertEqual(len(files), 1, "Should get EXACTLY ONE fits file")
+            self.assertEqual(len(file), 699840, "Should be 699840 bytes")
+
+            self.assertEqual(len(secondary_files), 2, "Should get EXACTLY TWO abc files")
+            for sec_file in secondary_files:
+                sec_file_phys = driver.get_physical_file(sec_file)
+                self.assertEqual(len(sec_file_phys), len(contents), "%s should have %d bytes" % (sec_file, len(contents)))
+
+        finally:
+            for file in array_files:
+                os.remove('{}/{}'.format(folder_name, file['name']))
 
 
 if __name__ == '__main__':
